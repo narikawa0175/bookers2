9d520b5dfb8fd66fdfa3b0280ac1658934504f30
EC2 Default User <ec2-user@ip-172-31-17-195.ap-northeast-1.compute.internal>
2022-05-07 09:23:53 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,33 @@
 class BooksController < ApplicationController
+  before_action :correct_user,only:[:edit,:update]
+ 
   def new
    @book=Book.new
   end
   
   def create
    @book=Book.new(book_params)
+   @user=current_user
+   @posts=Book.all
    @book.user_id=current_user.id
-   @book.save
-   redirect_to book_path(@book.id)
+   if @book.save
+    redirect_to book_path(@book.id),notice:"You have created book successfully."
+   else
+    render :index
+   end
   end
   
   def index
-   @post=Book.new
+   @book=Book.new
    @user=current_user
    @posts=Book.all
   end
 
   def show
-   @user=current_user
-   @post=Book.new
+   
+   @book=Book.new
    @posts=Book.find(params[:id])
+   @user=@posts.user
   end
 
   def edit
@@ -28,19 +36,28 @@ class BooksController < ApplicationController
   
   def update
    @book=Book.find(params[:id])
-   @book.update(book_params)
-   redirect_to book_path(@book.id)
+   if @book.update(book_params)
+    redirect_to book_path(@book.id),notice:"You have updated book successfully."
+   else
+    render :edit
+   end
   end
   
   def destroy
-   book=Book.find(params[:id])
-   book.destroy
-   redirect_to books_path(book.id)
+   @book=Book.find(params[:id])
+   @book.destroy
+   redirect_to books_path
   end
   
   private
   
    def book_params
     params.require(:book).permit(:title,:body)
+   end
+   
+   def correct_user
+    @book=Book.find(params[:id])
+    @user=@book.user
+    redirect_to(books_path) unless @user==current_user
    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
+  before_action :correct_user,only:[:edit,:update]
+  
   def index
    @user=current_user
-   @post=Book.new
+   @book=Book.new
    @users=User.all
   end
 
   def show
-   @user=current_user
-   @post=Book.new
+   @user=User.find(params[:id])
+   @book=Book.new
    @posts=@user.books
   end
 
@@ -17,13 +19,21 @@ class UsersController < ApplicationController
   
   def update
    @user=User.find(params[:id])
-   @user.update(user_params)
-   redirect_to user_path(@user.id)
+   if @user.update(user_params)
+    redirect_to user_path(@user.id),notice:"You have updated user successfully."
+   else
+    render :edit
+   end
   end
   
   private
   
   def user_params
    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+  
+  def correct_user
+   @user=User.find(params[:id])
+   redirect_to user_path(current_user.id) unless @user==current_user
   end
 end
